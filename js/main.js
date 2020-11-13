4fb5c608d9c5e0ee3e744d5899e176626b4f6741
k304189 <k304189@gmail.com>
2020-11-13 10:09:38 +0900

    addModalArea & modalFunction

--- js/main.js
+++ js/main.js
@@ -6,6 +6,33 @@ $(function(){
     let target = $(href === "#" || href === "" ? 'html' : href);
     let position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
+    toggleModal();
+    return false;
+  });
+
+  //ヘッダーリンクのモーダルクローズ
+  $("#menuOpen, #menuClose, #modal").click(function(){
+    toggleModal();
     return false;
   });
 });
+
+//モーダルのボタン切り替え
+function toggleModal() {
+  const modal = $("#modal");
+  const menuOpen = $("#menuOpen");
+  const menuClose = $("#menuClose");
+  const speed = 300;
+  const modalDisplay = modal.css('display');
+
+  if(modalDisplay === 'none') {
+    menuOpen.hide();
+    menuClose.show();
+  } else {
+    menuOpen.show();
+    menuClose.hide();
+  }
+
+  $("#modal").slideToggle(speed);
+  return false;
+}
