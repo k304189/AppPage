801f08eaee1aebe7e19cb1f1fd0b56d6e9562d3b
k304189 <k304189@gmail.com>
2020-11-14 09:45:58 +0900

    While Open Modal, Scroll fixed. And When Click Link, Show Section Title.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,81 @@
 $(function(){
   //スクロールアニメーション
   $('a[href^="#"]').click(function(){
-    let speed = 500;
     let href = $(this).attr("href");
-    let target = $(href === "#" || href === "" ? 'html' : href);
-    let position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    toggleModal();
+
+    if(href !== "#" && isOpenModal()) {
+      closeModal(); //モーダルクローズ
+    }
+    scrollSection(href); //スクロール移動
+
     return false;
   });
 
-  //ヘッダーリンクのモーダルクローズ
-  $("#menuOpen, #menuClose, #modal").click(function(){
-    toggleModal();
+  //モーダル表示ボタンクリック
+  $("#menuOpen").click(function(){
+    openModal();
     return false;
   });
+
+  //モーダル表示ボタンクリック
+  $("#menuClose, #modal").click(function(){
+    closeModal();
+    return false;
+  });
+
 });
 
-//モーダルのボタン切り替え
-function toggleModal() {
-  const modal = $("#modal");
-  const menuOpen = $("#menuOpen");
-  const menuClose = $("#menuClose");
-  const speed = 300;
-  const modalDisplay = modal.css('display');
+let scrollPosition;
 
-  if(modalDisplay === 'none') {
-    menuOpen.hide();
-    menuClose.show();
-  } else {
-    menuOpen.show();
-    menuClose.hide();
-  }
+//モーダルの表示確認
+function isOpenModal() {
+  const modalDisplay = $("#modal").css('display');
+  let isOpenModal = modalDisplay === 'block';
 
-  $("#modal").slideToggle(speed);
+  return isOpenModal;
+}
+
+//スクロールイベント
+function scrollSection(href) {
+  let speed = 500;
+  let target = $(href === "#" || href === "" ? 'html' : href);
+  let position = target.offset().top;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
   return false;
+}
+
+//モーダルオープン
+function openModal() {
+
+  //ボタン切り替え
+  $("#menuOpen").hide();
+  $("#menuClose").show();
+
+  //モーダル表示
+  $("#modal").slideDown(300);
+
+  //背景スクロール固定
+  scrollPosition = $(window).scrollTop();
+  $("body").css({
+    top: (-1 * scrollPosition),
+    position: "fixed"
+  });
+}
+
+//モーダルクローズ
+function closeModal() {
+  //ボタン切り替え
+  $("#menuOpen").show();
+  $("#menuClose").hide();
+
+  //モーダル表示
+  $("#modal").slideUp(300);
+
+  //背景スクロール解除
+  $("body").css({
+    top: 0,
+    position: "static"
+  });
+  $(window).scrollTop(scrollPosition);
+
 }
